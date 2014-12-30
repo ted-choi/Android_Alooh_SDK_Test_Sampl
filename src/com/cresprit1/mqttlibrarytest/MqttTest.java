9a17eb9637f86791a471f58ed255c6080dbe44fe
최창현 <ted.choi@chchoi-pc.(none)>
2014-12-30 21:32:41 +0900

    first commit

---
+++ src/com/cresprit1/mqttlibrarytest/MqttTest.java
@@ -0,0 +1,59 @@
+package com.cresprit1.mqttlibrarytest;
+
+
+import org.eclipse.paho.client.mqttv3.MqttException;
+
+import android.app.Activity;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+
+
+
+
+import com.cresprit.mqtt_sdk.IUpdateListener;
+//import com.cresprit.mqttlibrarytest.R;
+import com.cresprit.mqtt_sdk.MQTTSDK;;
+
+public class MqttTest extends Activity {
+	String LoginKey = null;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.mqtt_test_layout);
+		new task().execute("");
+		Log.i("","LoginKey : "+LoginKey);
+	}
+
+	class task extends AsyncTask{
+
+		@Override
+		protected Object doInBackground(Object... params) {
+			// TODO Auto-generated method stub
+			LoginKey = MQTTSDK.LogIn("tough45@naver.com", "aaaaaa");
+			Log.i("","LoginKey : "+LoginKey);
+			int result=0;
+			MQTTSDK.subscribe(MqttTest.this, "tesal", _listener);
+			//result=  MQTTSDK.Publish("tesal", "test");
+			Log.i("","LoginKey : "+LoginKey+" result : "+result);
+			
+			return null;
+		}
+		
+	}
+	
+	IUpdateListener _listener = new IUpdateListener()
+	{
+
+		public void update(String arg0) {
+			// TODO Auto-generated method stub
+			Log.i("",""+arg0);
+		}
+		
+	};
+}
