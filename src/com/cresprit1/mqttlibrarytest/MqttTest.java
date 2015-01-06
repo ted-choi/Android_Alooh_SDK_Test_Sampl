ea9d12cb2c37241ad26336309d0ce6693b3dbb9d
최창현 <ted.choi@chchoi-pc.(none)>
2015-01-06 21:03:40 +0900

    ing

--- src/com/cresprit1/mqttlibrarytest/MqttTest.java
+++ src/com/cresprit1/mqttlibrarytest/MqttTest.java
@@ -1,6 +1,8 @@
 package com.cresprit1.mqttlibrarytest;
 
 
+import java.util.Vector;
+
 import org.eclipse.paho.client.mqttv3.MqttException;
 
 import android.app.Activity;
@@ -15,13 +17,14 @@ import android.view.MenuItem;
 
 
 
+
 import com.cresprit.mqtt_sdk.IUpdateListener;
 //import com.cresprit.mqttlibrarytest.R;
 import com.cresprit.mqtt_sdk.MQTTSDK;;
 
 public class MqttTest extends Activity {
 	String LoginKey = null;
-	
+	Vector<MQTTSDK.DeviceInfo> deviceinfo = null;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -38,7 +41,8 @@ public class MqttTest extends Activity {
 			LoginKey = MQTTSDK.LogIn("tough45@naver.com", "aaaaaa");
 			Log.i("","LoginKey : "+LoginKey);
 			int result=0;
-			MQTTSDK.subscribe(MqttTest.this, "tesal", _listener);
+			deviceinfo = MQTTSDK.getDeviceList();
+			MQTTSDK.Subscribe(MqttTest.this, deviceinfo.get(0).getDeviceName(), _listener);
 			//result=  MQTTSDK.Publish("tesal", "test");
 			Log.i("","LoginKey : "+LoginKey+" result : "+result);
 			
